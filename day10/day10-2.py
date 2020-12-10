c6ea5c67b5b2861cc808ab980def448abca29b28
kat <k82019@gmail.com>
2020-12-10 01:49:24 -0500

    part two works faster now

--- day10/day10-2.py
+++ day10/day10-2.py
@@ -6,22 +6,34 @@ with open('advent2020/day10/day10input.txt', 'r') as file:
 
 nums.sort()
 nums.append(nums[-1]+3)
+threes = [0]
+print(nums)
 
-def find_potentials(currentNum):
+for numIndex in range(1,len(nums)):
+    diff = nums[numIndex] - nums[numIndex-1]
+    if diff == 3:
+        threes.append(nums[numIndex])
+
+def find_potentials(currentNum,endNum):
     global valid
-    if (currentNum == nums[-1]):
+    if (currentNum == endNum):
         return True
     if (currentNum + 1) in nums:
-        if find_potentials(currentNum + 1) == True:
+        if find_potentials(currentNum + 1,endNum) == True:
             valid += 1
     if (currentNum + 2) in nums:
-        if find_potentials(currentNum + 2) == True:
+        if find_potentials(currentNum + 2,endNum) == True:
             valid += 1
     if (currentNum + 3) in nums:
-        if find_potentials(currentNum + 3) == True:
+        if find_potentials(currentNum + 3,endNum) == True:
             valid += 1
     if not ((currentNum + 1) in nums and (currentNum + 2) in nums and (currentNum + 3) in nums):
         return False
 
-find_potentials(nums[0])
-print(valid)
+totalValid = 1
+for index in range(1,len(threes)):
+    valid = 0
+    find_potentials(threes[index - 1], threes[index])
+    totalValid *= valid
+    
+print(totalValid)
